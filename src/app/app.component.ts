d74ee979e7673cdbb6ae222621d5f50db387773f
Daniele Quintiliani <quintiliani.d@gmail.com>
2024-10-06 14:26:17 +0200

    refractoring html and app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { AboutComponent } from "./about/about.component";
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
